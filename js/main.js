b5f48bf6f9c3741931d3a020b5768a06c2765c25
huiwenxchen <97475388+huiwenxchen@users.noreply.github.com>
2022-01-10 16:04:00 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,6 @@ var genre = [];
 d3.csv("data/spotify_data.csv", function(data){
 
 	// Analyze the dataset in the web console
-
 	var preparedData = prepareData(data);
 
 
@@ -23,13 +22,8 @@ d3.csv("data/spotify_data.csv", function(data){
 });
 
 var prepareData = function(data) {
-	// Step 1: Analyze and prepare the dataelationship satisfaction 
-	// Use the web console to get a better idea of the dataset
-	// Convert numeric values to numbers.
-	// Make sure the genres array has the name of each genre
-
-	// console.log(data);
 	
+	// Analyze and prepare the dataelationship satisfaction 
 	data.forEach( function (d) {
 		d["streams_in_mils"] = parseInt(d["streams_in_mils"]);
 		d["songs"] = parseInt(d["songs"]);
@@ -45,18 +39,6 @@ var prepareData = function(data) {
 	console.log(data)
 
 	return data;
-
-	// data = data.filter(function(d){
-	// 	return false;
-	// });
-
-    // return {
-    //  	name: d["name"],
-	// 	streams_in_mils: parseInt[d.streams_in_mils],
-	// 	songs: parseInt[d.songs],
-	// 	songs_with_mil_plus_streams: parseInt[d.songs_with_mil_plus_streams],
-	// 	genre: d["genre"],
-    // };
 }
 
 // Initiate margin convention 
@@ -69,48 +51,28 @@ var width = 960 - margin.left - margin.right,
 
 
 var createVisualization = function(data) {
-	// Step 2: Append a new SVG area with D3
-	// The ID of the target div container in the html file is #chart-area
-	// Use the margin convention with 50 px of bottom margin and 30 px of margin on other sides!
 	
-    var svg = d3.select("#chart-area").append("svg")
+	// Append a new SVG area with D3
+	var svg = d3.select("#chart-area").append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            	.append("g")
+            	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	// Step 3: Create linear scales by using the D3 scale functions
-	// You will need an songs scale (x-axis) and a scale function for the streams (y-axis).
-	// Call them numSongsScale and streamsScale.
-	// Use d3.min() and d3.max() (or d3.extent()) for the input domain
-	// Use the variables height and width for the output range
-    var numSongsScale = d3.scaleLinear()
+	// create scales by using the D3 scale functions
         .domain(d3.extent(data, function (d){
             return d.songs;
         }))
         .range([padding, width - padding]);
 
-    var streamsScale = d3.scaleLinear()
+	var streamsScale = d3.scaleLinear()
         .domain(d3.extent(data, function (d){
             return d.streams_in_mils;
         }))
         .range([height - padding, padding]);
 
-	// Step 4: Try the scale functions
-	// You can call the functions with example values and print the result to the web console.
 
-
-
-
-
-
-
-	// Step 6: Use your scales (songs and streams) to create D3 axis functions
-
-	// Step 7: Append the x- and y-axis to your scatterplot
-	// Add the axes to a group element that you add to the SVG drawing area.
-	// Use translate() to change the axis position
-
+	// Append the x- and y-axis to your scatterplot
 	var xAxis = d3.axisBottom(numSongsScale);
 	svg.append("g")
 		.attr("class", "axis xAxis")
@@ -126,16 +88,7 @@ var createVisualization = function(data) {
 		.call(yAxis);
 
 
-
-
-
-	// Step 8: Refine the domain of the scales
-	// Notice that some of the circles are positioned on the outer edges of the svg area
-	// You can include buffer values to widen the domain and to prevent circles and axes from overlapping
-
-
-	// Step 9: Label your axes
-
+	// Label axes
 	svg.append("text")
 		.attr("class", "x label")
 		.attr("text-anchor", "end")
@@ -154,39 +107,20 @@ var createVisualization = function(data) {
 		.text("NUmber of streams in Millions");
 
 
-	// Step 10: Add a scale function for the circle radius
-	// Create a linear scale function dependent on the number of million plus streamed songs
-	// The radius should be between 4 - 30px.
-	// Then use the scale function to specify a dynamic radius
+	// Add a scale function for the circle radius	
 	let rScale = d3.scaleLinear()
 		.domain(d3.extent(data, function(d){
 			return d.songs_with_mil_plus_streams;
 		}))
 		.range([4, 30]);
-
 	
-
-	// Step 5: Map the countries to SVG circles
-	// Use select(), data(), enter() and append()
-	// Instead of setting the x- and y-values directly,
-	// you have to use your scale functions to convert the data values to pixel measures
-	// let colorRange = d3.parseInt(d["genre"]);
-	// let extent4 = d3.extent(data, function (d){
-	// 	return colorRange;
-	// });
-	// var colors = d3.scaleOrdinal()
-	// .domain(extent4)
-	// .range(["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", "#FFFFBF", "#FEE08B",]);
-
-	// var gradientColor = d3.scaleOrdinal()
-	// 	.domain([rap, pop, latin, r&b, edm, rock, metal, reggae])
-	// 	.range(["#de9ed6","#ce6dbd","#a55194","#7b4173", "#e7969c", "#d6616b", "#ad494a", " #843c39"]);
-
+	// Color
 	let Colorscale = d3.scaleOrdinal()
 		.domain(d3.extent(data, function(d){
 		return d.genre;
 	}))
 		.range(["#de9ed6","#ce6dbd","#a55194","#7b4173", "#e7969c", "#d6616b", "#ad494a", " #843c39"])
+	
 	svg.selectAll("circle")
         .data(data)
         .enter()
@@ -205,20 +139,5 @@ var createVisualization = function(data) {
 			
 	
 		})
-
-
-	  // data.sort(function(x,y) {
-    //     return d3.descending(x.population, y.population);
-    // })
-	// Step 11: Change the drawing order
-	// Larger circles should not overlap or cover smaller circles.
-	// Sort the artists by streams before drawing them.
-
-
-	// Step 12: Color the circles depending on their genres
-	// Use a D3 color scale
-
-
-
 
 }
